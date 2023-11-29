094ae27d3f9629a4bed3bb4c02b97e4eeb9f2158
JesusSalvadorSierraArballo <jesusssierraa@gmail.com>
2023-11-28 18:03:12 -0600

    Feature(xchange-icons): Add send icon

--- projects/ngx-xchange-icons/src/lib/send.component.ts
+++ projects/ngx-xchange-icons/src/lib/send.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'xc-send',
+  selector: 'xc-icon-send',
   standalone:true,
   template: `
-<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-<path d="M22 2L11 13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22 2L15 22L11 13L2 9L22 2Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`,
+<svg width="46" height="47" viewBox="0 0 46 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2998_16768)">
+<path d="M42.2283 23.4998H21.4865" stroke="#111111" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M42.2282 23.4968L16.7724 35.7533L21.4865 23.4968L16.7724 11.2403L42.2282 23.4968Z" stroke="#111111" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_2998_16768">
+<rect width="32" height="32" fill="white" transform="translate(23.3721 0.871094) rotate(45)"/>
+</clipPath>
+</defs>
+</svg>
+
+
+`,
   styles: [
   ]
 })
