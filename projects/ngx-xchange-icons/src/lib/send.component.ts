f5030e6d76721163a4f5b4e2db454e9e19e48b5b
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2024-10-08 19:56:50 -0600

    change aside nav color selector method

--- projects/ngx-xchange-icons/src/lib/send.component.ts
+++ projects/ngx-xchange-icons/src/lib/send.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'xc-icon-send',
   standalone:true,
   template: `
-<svg viewBox="0 0 46 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="46" height="47" viewBox="0 0 46 47" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_2998_16768)">
 <path d="M42.2283 23.4998H21.4865" stroke="#111111" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M42.2282 23.4968L16.7724 35.7533L21.4865 23.4968L16.7724 11.2403L42.2282 23.4968Z" stroke="#111111" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
