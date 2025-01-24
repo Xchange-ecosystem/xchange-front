6dff884d796dbfc98d0ae8198ed0ba3513a7d8fb
manuXchange <josemanuelcabrera2011@gmail.com>
2025-01-24 10:53:30 -0600

    change main headin directive

--- projects/ngx-xchange-ui/src/directives/main-heading/styles.ts
+++ projects/ngx-xchange-ui/src/directives/main-heading/styles.ts
@@ -40,14 +40,14 @@ export const APP_TAILWIND_STYLES: { [key: string]: string } = {
   font-medium
   leading-more
   `,
-  sixthHeading:`
+  sixthHeading: `
   text-paragraph
   text-Main_Fonts
   font-manrope
   font-bold
   leading-filter-bar
   `,
-  seventh:`
+  seventh: `
   text-cardsTitles
   text-white
   font-manrope
@@ -60,5 +60,93 @@ export const APP_TAILWIND_STYLES: { [key: string]: string } = {
   font-manrope 
   font-semibold 
   leading-[32px]
-  `
+  `,
+  Headline500: `
+  font-manrope
+  not-italic
+  text-Headline-500-font-size
+  font-Headline-500-font-weight
+  leading-Headline-500-line-height
+  text-Black400
+  `,
+  Headline400: `
+  font-manrope
+  not-italic
+  text-Headline-400-font-size
+  font-Headline-400-font-weight
+  leading-Headline-400-line-height
+  text-Black400
+  `,
+  Headline300: `
+  font-manrope
+  not-italic
+  text-Headline-300-font-size
+  font-Headline-300-font-weight
+  leading-Headline-300-line-height
+  text-Black400
+  `,
+  Headline200: `
+  font-manrope
+  not-italic
+  text-Headline-200-font-size
+  font-Headline-200-font-weight
+  leading-Headline-200-line-height
+  text-Black400
+  `,
+  Copy400Regular: `
+  font-manrope
+  not-italic
+  text-Copy-400-Regular-font-size
+  font-Copy-400-Regular-font-weight
+  leading-Copy-400-Regular-line-height
+  text-Black500
+  `,
+  Copy400Medium: `
+  font-manrope
+  not-italic
+  text-Copy-400-Medium-font-size
+  font-Copy-400-Medium-font-weight
+  leading-Copy-400-Medium-line-height
+  text-Black500
+  `,
+  Copy400Bold: `
+  font-manrope
+  not-italic
+  text-Copy-400-Bold-font-size
+  font-Copy-400-Bold-font-weight
+  leading-Copy-400-Bold-line-height
+  text-Black400
+  `,
+  Copy300Medium: `
+  font-manrope
+  not-italic
+  text-Copy-300-Medium-font-size
+  font-Copy-300-Medium-font-weight
+  leading-Copy-300-Medium-line-height
+  text-Black400
+  `,
+  Copy300Bold: `
+  font-manrope
+  not-italic
+  text-Copy-300-Bold-font-size
+  font-Copy-300-Bold-font-weight
+  leading-Copy-300-Bold-line-height
+  text-Black400
+  `,
+  Copy200Medium: `
+  font-manrope
+  not-italic
+  text-Copy-200-Medium-font-size
+  font-Copy-200-Medium-font-weight
+  leading-Copy-200-Medium-line-height
+  text-Black400
+  `,
+  Copy200Bold: `
+  font-manrope
+  not-italic
+  text-Copy-300-Bold-font-size
+  font-Copy-300-Bold-font-weight
+  leading-Copy-300-Bold-line-height
+  text-Black400
+  `,
 };
