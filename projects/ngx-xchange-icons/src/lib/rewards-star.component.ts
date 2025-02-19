a4db9c4518b61481bf4ddd97fcdd0d4de9227cfc
manuXchange <josemanuelcabrera2011@gmail.com>
2025-02-19 09:09:10 -0600

    change with and color of every svg file in lib

--- projects/ngx-xchange-icons/src/lib/rewards-star.component.ts
+++ projects/ngx-xchange-icons/src/lib/rewards-star.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'xc-icon-rewards-start',
   standalone:true,
   template: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24"  fill="none" width="100%" height="100%">
 <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" stroke="#5D5D5D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `,
--- projects/ngx-xchange-icons/src/lib/web-section-alt.component.ts
+++ projects/ngx-xchange-icons/src/lib/web-section-alt.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'xc-icon-web-section-alt',
   standalone:true,
   template: `
-    <svg width="24" height="24" viewBox="0 0 24 24" undefinedxmlns="http://www.w3.org/2000/svg">
+    <svg   viewBox="0 0 24 24" undefinedxmlns="http://www.w3.org/2000/svg" fill="none" width="100%" height="100%">
 <path d="M8 20.5H8.5V20V4V3.5H8H4H3.5V4V20V20.5H4H8ZM20 20.5H20.5V20V4V3.5H20H10H9.5V4V20V20.5H10H20ZM3 2.5H21C21.1326 2.5 21.2598 2.55268 21.3536 2.64645C21.4473 2.74021 21.5 2.86739 21.5 3V21C21.5 21.1326 21.4473 21.2598 21.3536 21.3536C21.2598 21.4473 21.1326 21.5 21 21.5H3C2.86739 21.5 2.74021 21.4473 2.64645 21.3536C2.55268 21.2598 2.5 21.1326 2.5 21V3C2.5 2.86739 2.55268 2.74021 2.64645 2.64645C2.74021 2.55268 2.86739 2.5 3 2.5Z" />
 </svg>
 `,
