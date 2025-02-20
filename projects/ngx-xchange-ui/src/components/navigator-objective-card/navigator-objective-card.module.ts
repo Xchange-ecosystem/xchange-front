ad99a1081523eb26e961bd723cb320f5048fa22d
Jesús Sierra <jesusssierraa@gmail.com>
2025-02-20 06:45:08 -0600

    Fix: add navigator-objective-card component to public-api and solve circular reference

--- projects/ngx-xchange-ui/src/components/navigator-objective-card/navigator-objective-card.module.ts
+++ projects/ngx-xchange-ui/src/components/navigator-objective-card/navigator-objective-card.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, NgClass } from '@angular/common';
 import { NavigatorObjectiveCardComponent } from './navigator-objective-card/navigator-objective-card.component';
-import { CardTasksIndicatorModule, MainHeadingModule } from '@indziaki/ngx-xchange-ui';
+import { CardTasksIndicatorModule } from '../card-tasks-indicator/card-tasks-indicator.module';
+import { MainHeadingModule } from '../../directives/main-heading/main-heading.module';
 import { XcIconCalendarAltComponent, XcIconFileCheckAltComponent, XcIconPaperclipComponent } from '@indziaki/ngx-xchange-icons';
-
-
 
 @NgModule({
   declarations: [
