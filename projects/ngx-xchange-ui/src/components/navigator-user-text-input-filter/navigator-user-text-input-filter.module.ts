937f5ebc59c7d20dc164e5559af5e122d86d575a
manuXchange <josemanuelcabrera2011@gmail.com>
2025-02-24 17:15:37 -0600

    add navigator text input component

---
+++ projects/ngx-xchange-ui/src/components/navigator-user-text-input-filter/navigator-user-text-input-filter.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigatorUserTextInputFilterComponent } from './navigator-user-text-input-filter/navigator-user-text-input-filter.component';
+import { XcIconCheckComponent, XcIconTimesComponent } from '@indziaki/ngx-xchange-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgKeyupModule } from '../../directives/ng-keyup/ng-keyup.module';
+
+
+
+@NgModule({
+  declarations: [
+    NavigatorUserTextInputFilterComponent
+  ],
+  imports: [
+    CommonModule,
+    XcIconTimesComponent,
+    XcIconCheckComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    NgKeyupModule
+  ],
+  exports: [
+    NavigatorUserTextInputFilterComponent
+  ]
+})
+export class NavigatorUserTextInputFilterModule { }
