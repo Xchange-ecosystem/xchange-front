1a29fa23858185c2f2f8e6a16d4c280954817343
Jesús Sierra <jesusssierraa@gmail.com>
2025-03-03 14:47:54 -0600

    Fix: Solve import to public api

--- projects/ngx-xchange-ui/src/components/objective-options/objective-options.module.ts
+++ projects/ngx-xchange-ui/src/components/objective-options/objective-options.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObjectiveOptionsComponent } from './objective-options/objective-options.component';
-import { MainHeadingModule } from 'dist/ngx-xchange-ui/public-api';
 import { XcIconAddComponent, XcIconCopyComponent, XcIconEditComponent, XcIconFileShareAltComponent, XcIconTrashComponent } from '@indziaki/ngx-xchange-icons';
+import { MainHeadingModule } from '../../public-api';
 
 
 
