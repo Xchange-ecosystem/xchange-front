3baec5bdca68eb067aa62c4ec556bc2559e9965a
manuXchange <josemanuelcabrera2011@gmail.com>
2025-03-04 07:08:14 -0600

    fix icon reference in user assessment card

--- projects/ngx-xchange-ui/src/components/user-assessment-card/user-assessment-card.module.ts
+++ projects/ngx-xchange-ui/src/components/user-assessment-card/user-assessment-card.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserAssessmentCardComponent } from './user-assessment-card/user-assessment-card.component';
 import { RoundedImageModule } from '../rounded-image/rounded-image.module';
-import { XcIconClockFiveComponent } from '@indziaki/ngx-xchange-icons';
+import { XcIconAngleRightBComponent, XcIconClockFiveComponent } from '@indziaki/ngx-xchange-icons';
 import { RatingBarModule } from '../rating-bar/rating-bar.module';
-import { XcIconAngleRightBComponent } from 'projects/ngx-xchange-icons/src/public-api';
 import { TagModule } from '../../directives/tag/tag.module';
 
 
