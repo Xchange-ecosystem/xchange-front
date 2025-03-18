e3eac4c2aa2220627d995cb7aa25c4ae748a9e5e
Damian Tapia <damian.tapia45@gmail.com>
2025-03-18 08:55:03 -0600

    add: function to change slide

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  selectedOption: 'login' | 'signup' = 'login';
 
+  toggleSlide(option: 'login' | 'signup') {
+    this.selectedOption = option;
+  }
 }
