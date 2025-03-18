5f9d010cf50b88a791298ddec310b1eec16bac47
Damian Tapia <damian.tapia45@gmail.com>
2025-03-18 14:55:23 -0600

    add: modal opening on clic

--- projects/ngx-xchange-ui/src/components/community-cards/community-cards.component.ts
+++ projects/ngx-xchange-ui/src/components/community-cards/community-cards.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { XcIconEnvelopeAltComponent } from 'projects/ngx-xchange-icons/src/lib/envelope-alt.component';
 import { CardMeasuresDirective } from 'projects/ngx-xchange-ui/src/directives/card-measures/card-measures.directive';
-import {ProfileTagsDirective} from 'projects/ngx-xchange-ui/src/directives/profile-tags/profile-tags.directive';
+import { ProfileTagsDirective } from 'projects/ngx-xchange-ui/src/directives/profile-tags/profile-tags.directive';
 import { RoundedImageModule } from 'projects/ngx-xchange-ui/src/components/rounded-image/rounded-image.module';
 import { MainHeadingModule } from '../../directives/main-heading/main-heading.module';
 import { NgFor, NgIf } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { UserProfileModalComponent } from '../user-profile-modal/user-profile-modal.component';
 @Component({
   selector: 'xc-community-cards',
   standalone: true,
@@ -17,30 +18,56 @@ import { RouterLink } from '@angular/router';
     MainHeadingModule,
     NgFor,
     NgIf,
-],
+    UserProfileModalComponent
+  ],
   templateUrl: './community-cards.component.html',
   styleUrl: './community-cards.component.css',
 })
 export class CommunityCardsComponent {
   public users = [
     {
+      id:1,
       name: 'John Doe',
-      imageUrl: 'https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b',
+      imageUrl:
+        'https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b',
       tags: ['Healt', 'Entrepeneur', 'Lorem'],
       description: 'This is a small description of the user',
       collaborations: 2,
-      email:"tester@gmail.com",
-      linkedin:"https://www.linkedin.com/in/john-doe"
+      email: 'tester@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/john-doe',
     },
     {
+      id:2,
       name: 'Jenna Ortega',
       imageUrl: 'https://xsgames.co/randomusers/assets/avatars/male/36.jpg',
       tags: ['Actress', 'Entrepeneur', 'Lorem'],
       description:
         'I am a alternative practitioner, hormone and nutrient coach. My vision is to make complete, all-encompassing, holistic and integrative health knowledge accessible to all people. Thats why we founded OneLife',
       collaborations: 200,
-      email:"tester@gmail.com",
-      linkedin:null
+      email: 'tester@gmail.com',
+      linkedin: null,
+    },
+    {
+      id:3,
+      name: 'Manuel Cabrera',
+      imageUrl: 'https://xsgames.co/randomusers/assets/avatars/male/36.jpg',
+      tags: ['Actress', 'Entrepeneur', 'Lorem'],
+      description:
+        'This is a small description of the user just a little bit changed for dev purposses',
+      collaborations: 200,
+      email: 'tester@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/john-doe',
+    },
+    {
+      id:4,
+      name: 'Manuel Cabrera',
+      imageUrl: 'https://xsgames.co/randomusers/assets/avatars/male/36.jpg',
+      tags: ['Actress', 'Entrepeneur', 'Lorem'],
+      description:
+        'This is a small description of the user just a little bit changed for dev purposses',
+      collaborations: 200,
+      email: 'tester@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/john-doe',
     },
     {
       name: 'Manuel Cabrera',
@@ -49,28 +76,18 @@ export class CommunityCardsComponent {
       description:
         'This is a small description of the user just a little bit changed for dev purposses',
       collaborations: 200,
-      email:"tester@gmail.com",
-      linkedin:"https://www.linkedin.com/in/john-doe"
-    },
-    {
-      name: 'Manuel Cabrera',
-      imageUrl: 'https://xsgames.co/randomusers/assets/avatars/male/36.jpg',
-      tags: ['Actress', 'Entrepeneur', 'Lorem'],
-      description:
-        'This is a small description of the user just a little bit changed for dev purposses',
-      collaborations: 200,
-      email:"tester@gmail.com",
-      linkedin:"https://www.linkedin.com/in/john-doe"
-    },
-    {
-      name: 'Manuel Cabrera',
-      imageUrl: 'https://xsgames.co/randomusers/assets/avatars/male/36.jpg',
-      tags: ['Actress', 'Entrepeneur', 'Lorem'],
-      description:
-        'This is a small description of the user just a little bit changed for dev purposses',
-      collaborations: 200,
-      email:"tester@gmail.com",
-      linkedin:"https://www.linkedin.com/in/john-doe"
+      email: 'tester@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/john-doe',
     },
   ];
+
+  selectedUser: any = null;
+
+  openModal(user: any): void {
+    this.selectedUser = user;
+  }
+
+  closeModal(): void {
+    this.selectedUser = null;
+  }
 }
--- projects/ngx-xchange-ui/src/components/user-profile-modal/user-profile-modal.component.ts
+++ projects/ngx-xchange-ui/src/components/user-profile-modal/user-profile-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProfileTagsDirective } from '../../directives/profile-tags/profile-tags.directive';
 import { RoundedImageModule } from '../rounded-image/rounded-image.module';
 import { NgFor } from '@angular/common';
@@ -12,21 +12,5 @@ import { ProfileHeaderDirective } from '../../directives/profile-header/profile-
   styleUrl: './user-profile-modal.component.css'
 })
 export class UserProfileModalComponent {
-  public users = [
-    {
-      name: 'John Doe',
-      company:'COD Company',
-      completedObj:12,
-      activeObj:12,
-      nextSteps:0o3,
-      invProyects:4,
-      collaborations: 2,
-      imageUrl: 'https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b',
-      tags: ['Healt', 'Entrepeneur', 'Lorem','Entrepeneur','Entrepeneur',],
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      memberDate:"7 Aug 2024",
-      email:"tester@gmail.com",
-      linkedin:"https://www.linkedin.com/in/john-doe"
-    },
-  ];
+  @Input() user:any
 }
