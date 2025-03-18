bd16aa8c4bddb6e670b9fac5e491b73fceabfc87
Damian Tapia <damian.tapia45@gmail.com>
2025-03-18 12:30:28 -0600

    fix: directive on card-measures used on community-cards

--- projects/ngx-xchange-ui/src/directives/card-measures/card-measures.directive.ts
+++ projects/ngx-xchange-ui/src/directives/card-measures/card-measures.directive.ts
@@ -10,21 +10,21 @@ export class CardMeasuresDirective{
   @Input() set xcCardMeasures(klass: string | number) {
     this.cssClass = APP_CSS_STYLES[klass] || APP_CSS_STYLES['userCard'];
   }
-  // constructor(private el: ElementRef, private renderer: Renderer2) {
-  //   this.setStyles();
-  // }
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.setStyles();
+  }
 
-  // private setStyles(): void {
-  //   this.renderer.setStyle(this.el.nativeElement, 'display', 'flex');
-  //   this.renderer.setStyle(this.el.nativeElement, 'flex-direction', 'column');
-  //   this.renderer.setStyle(this.el.nativeElement, 'justify-content', 'space-between');
-  //   this.renderer.setStyle(this.el.nativeElement, 'align-items', 'center');
-  //   this.renderer.setStyle(this.el.nativeElement, 'gap', '16px');
-  //   this.renderer.setStyle(this.el.nativeElement, 'padding', '32px 32px 24px 32px');
-  //   this.renderer.setStyle(this.el.nativeElement, 'width', '500px');
-  //   this.renderer.setStyle(this.el.nativeElement, 'height', '500px');
-  //   this.renderer.setStyle(this.el.nativeElement, 'border-radius', '8px');
-  //   this.renderer.setStyle(this.el.nativeElement, 'background', 'var(--xc-white)');
-  //   this.renderer.setStyle(this.el.nativeElement, 'box-shadow', '0px 0px 30px rgba(17, 17, 17, 0.05)');
-  // }
+  private setStyles(): void {
+    this.renderer.setStyle(this.el.nativeElement, 'display', 'flex');
+    this.renderer.setStyle(this.el.nativeElement, 'flex-direction', 'column');
+    this.renderer.setStyle(this.el.nativeElement, 'justify-content', 'space-between');
+    this.renderer.setStyle(this.el.nativeElement, 'align-items', 'center');
+    this.renderer.setStyle(this.el.nativeElement, 'gap', '16px');
+    this.renderer.setStyle(this.el.nativeElement, 'padding', '32px 32px 24px 32px');
+    this.renderer.setStyle(this.el.nativeElement, 'width', '500px');
+    this.renderer.setStyle(this.el.nativeElement, 'height', '500px');
+    this.renderer.setStyle(this.el.nativeElement, 'border-radius', '8px');
+    this.renderer.setStyle(this.el.nativeElement, 'background', 'var(--xc-white)');
+    this.renderer.setStyle(this.el.nativeElement, 'box-shadow', '0px 0px 30px rgba(17, 17, 17, 0.05)');
+  }
 }
