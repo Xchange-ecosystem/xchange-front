2ef95e4b082a534430e24dae3373d590189b33fb
Damian Tapia <damian.tapia45@gmail.com>
2025-03-27 10:43:21 -0600

    fix:overflow-x with @media

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -9,10 +9,7 @@ import {
 } from '@indziaki/ngx-xchange-icons';
 import { AiFilterComponent } from 'projects/ngx-xchange-ui/src/components/ai-filter/ai-filter.component';
 import { ChatOptionsButtonComponent } from 'projects/ngx-xchange-ui/src/components/chat-options-button/chat-options-button.component';
-import { FeedButtonsComponent } from 'projects/ngx-xchange-ui/src/components/feed-buttons/feed-buttons.component';
 import { FeedObjectiveComponent } from 'projects/ngx-xchange-ui/src/components/feed-objective/feed-objective.component';
-import { NewButton400Component } from 'projects/ngx-xchange-ui/src/components/new-button-400/new-button-400.component';
-import { NewButton500Component } from 'projects/ngx-xchange-ui/src/components/new-button-500/new-button-500.component';
 import { UserHeaderComponent } from 'projects/ngx-xchange-ui/src/components/user-header/user-header.component';
 import { Button400Directive } from 'projects/ngx-xchange-ui/src/directives/button-400/button-400.directive';
 import { Button500Directive } from 'projects/ngx-xchange-ui/src/directives/button-500/button-500.directive';
