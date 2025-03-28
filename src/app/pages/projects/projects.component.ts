1736ef8c49612d9725bf0d8e31e47d00fcc1fb20
Damian Tapia <damian.tapia45@gmail.com>
2025-03-28 11:43:45 -0600

    fix: sort-by component styles/padding & gap

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
 import { FeedButtonDirective } from 'projects/ngx-xchange-ui/src/directives/feed-button/feed-button.directive';
+import { Headline600Directive } from 'projects/ngx-xchange-ui/src/directives/headline-600/headline-600.directive';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SortByComponent,FeedButtonDirective],
+  imports: [SortByComponent,FeedButtonDirective,Headline600Directive],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
