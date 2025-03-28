426bf96cc06ccc98d33e04fd284612d48dd834de
Damian Tapia <damian.tapia45@gmail.com>
2025-03-28 10:51:19 -0600

    add: navbar buttons & filters / fixed h2 to h5

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
+import { FeedButtonDirective } from 'projects/ngx-xchange-ui/src/directives/feed-button/feed-button.directive';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SortByComponent],
+  imports: [SortByComponent,FeedButtonDirective],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
