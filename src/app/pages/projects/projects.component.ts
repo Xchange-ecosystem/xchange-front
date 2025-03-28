a04e7c4ea2024d69dc5220ccbb6152632d3f831d
Damian Tapia <damian.tapia45@gmail.com>
2025-03-27 21:39:16 -0600

    add:filter bar component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [SortByComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
