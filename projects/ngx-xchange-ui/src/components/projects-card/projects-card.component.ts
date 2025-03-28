06a2b220c3d7909cc93e5af9011ebf60532ca20d
Damian Tapia <damian.tapia45gmail.com>
2025-03-28 13:32:02 -0600

    add: projects card

---
+++ projects/ngx-xchange-ui/src/components/projects-card/projects-card.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { ProfileTagsDirective } from '../../directives/profile-tags/profile-tags.directive';
+import { ProjectProgressBarModule } from '@indziaki/ngx-xchange-ui';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'xc-projects-card',
+  standalone: true,
+  imports: [ProfileTagsDirective, ProjectProgressBarModule, NgFor],
+  templateUrl: './projects-card.component.html',
+  styleUrl: './projects-card.component.css'
+})
+export class ProjectsCardComponent {
+  public projects = [
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzIuKHHeS7DE9kplbdGaHTZlQL92-_x8j4Bw&s",
+      title: "Project A",
+      completed: 20,
+      open: 10,
+      suggested: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Health", "Entrepreneur"],
+    },
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzIuKHHeS7DE9kplbdGaHTZlQL92-_x8j4Bw&s",
+      title: "Project B",
+      completed: 50,
+      open: 20,
+      suggested: 5,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Health", "Entrepreneur"],
+    },
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzIuKHHeS7DE9kplbdGaHTZlQL92-_x8j4Bw&s",
+      title: "Project C",
+      completed: 80,
+      open: 30,
+      suggested: 10,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Health", "Entrepreneur"],
+    }
+  ]
+
+  selectedProject: any = null;
+
+  openModal(project: any) {
+    this.selectedProject = project;
+  }
+  closeModal() {
+    this.selectedProject = null;
+  }
+}
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProjectsCardComponent } from 'projects/ngx-xchange-ui/src/components/projects-card/projects-card.component';
 import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
 import { FeedButtonDirective } from 'projects/ngx-xchange-ui/src/directives/feed-button/feed-button.directive';
 import { Headline600Directive } from 'projects/ngx-xchange-ui/src/directives/headline-600/headline-600.directive';
@@ -6,7 +7,7 @@ import { Headline600Directive } from 'projects/ngx-xchange-ui/src/directives/hea
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SortByComponent,FeedButtonDirective,Headline600Directive],
+  imports: [SortByComponent, FeedButtonDirective, Headline600Directive, ProjectsCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
