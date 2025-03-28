9c32b0b0bbe480538f6479a466704da323437026
Damian Tapia <damian.tapia45@gmail.com>
2025-03-28 11:17:24 -0600

    add:headline title-600

---
+++ projects/ngx-xchange-ui/src/directives/headline-600/headline-600.directive.spec.ts
@@ -0,0 +1,8 @@
+import { Headline600Directive } from './headline-600.directive';
+
+describe('Headline600Directive', () => {
+  it('should create an instance', () => {
+    const directive = new Headline600Directive();
+    expect(directive).toBeTruthy();
+  });
+});
