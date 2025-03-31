d47050b0cd32eb43d1a55be735661e856210f882
Damian Tapia <damian.tapia45gmail.com>
2025-03-31 12:28:49 -0600

    add: steps to project modal

---
+++ projects/ngx-xchange-ui/src/directives/headline-400/headline-400.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[xcHeadline400]',
+  standalone: true
+})
+export class Headline400Directive {
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.setStyle();
+  }
+
+  private setStyle() {
+    this.renderer.setStyle(this.el.nativeElement, 'font-size', '24px');
+    this.renderer.setStyle(this.el.nativeElement, 'font-weight', '600');
+    this.renderer.setStyle(this.el.nativeElement, 'color', 'var(--xc-black400)');
+  }
+}
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { XcIconPlusCircleComponent } from '@indziaki/ngx-xchange-icons';
-import { CreateProjectComponent } from 'projects/ngx-xchange-ui/src/components/create-project/create-project.component';
 import { ProjectsCardComponent } from 'projects/ngx-xchange-ui/src/components/projects-card/projects-card.component';
 import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
+import { Headline400Directive } from 'projects/ngx-xchange-ui/src/directives/headline-400/headline-400.directive';
 import { Headline600Directive } from 'projects/ngx-xchange-ui/src/directives/headline-600/headline-600.directive';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SortByComponent, Headline600Directive, ProjectsCardComponent, XcIconPlusCircleComponent, CreateProjectComponent],
+  imports: [SortByComponent, Headline600Directive, ProjectsCardComponent, XcIconPlusCircleComponent, Headline400Directive],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
