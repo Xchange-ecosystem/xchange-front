7febd14d0988da20543f719a94d578bda9a97302
Damian Tapia <damian.tapia45gmail.com>
2025-03-31 14:30:09 -0600

    style: project modal first step

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,24 +2,33 @@ import { Component } from '@angular/core';
 import { XcIconPlusCircleComponent } from '@indziaki/ngx-xchange-icons';
 import { ProjectsCardComponent } from 'projects/ngx-xchange-ui/src/components/projects-card/projects-card.component';
 import { SortByComponent } from 'projects/ngx-xchange-ui/src/components/sort-by/sort-by.component';
+import { FeedButtonDirective } from 'projects/ngx-xchange-ui/src/directives/feed-button/feed-button.directive';
 import { Headline400Directive } from 'projects/ngx-xchange-ui/src/directives/headline-400/headline-400.directive';
 import { Headline600Directive } from 'projects/ngx-xchange-ui/src/directives/headline-600/headline-600.directive';
+import { ClickOutsideDirective } from 'projects/ngx-xchange-ui/src/public-api';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SortByComponent, Headline600Directive, ProjectsCardComponent, XcIconPlusCircleComponent, Headline400Directive],
+  imports: [
+    SortByComponent, 
+    Headline600Directive, 
+    ProjectsCardComponent, 
+    XcIconPlusCircleComponent, 
+    Headline400Directive, 
+    FeedButtonDirective, 
+    ClickOutsideDirective],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-isModalOpen = false;
+  isModalOpen = false;
 
-openModal(): void {
-  this.isModalOpen = true;
-}
+  openModal(): void {
+    this.isModalOpen = true;
+  }
 
-closeModal(): void {
-  this.isModalOpen = false; 
-}
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
 }
