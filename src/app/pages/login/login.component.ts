3918856daf9dde5f0010255b000601bd903f89e7
Damian Tapia <damian.tapia45@gmail.com>
2025-04-02 11:14:20 -0600

    style:corrected/connection login-signup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
