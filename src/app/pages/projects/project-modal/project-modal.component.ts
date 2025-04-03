0f50cfa5bb90d49ef4311c6877ae750ce866b412
Damian Tapia <damian.tapia45@gmail.com>
2025-04-03 12:53:25 -0600

    style: modal content finish, header & button pending

--- src/app/pages/projects/project-modal/project-modal.component.ts
+++ src/app/pages/projects/project-modal/project-modal.component.ts
@@ -1,19 +1,32 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { XcIconArrowRightComponent } from '@indziaki/ngx-xchange-icons';
+import { XcIconArrowRightComponent, XcIconCheckComponent, XcIconCloseSComponent } from '@indziaki/ngx-xchange-icons';
 import { Headline300Directive } from 'projects/ngx-xchange-ui/src/directives/headline-300/headline-300.directive';
 import { Headline500Directive } from 'projects/ngx-xchange-ui/src/directives/headline-500/headline-500.directive';
-import { ProfileTagsDirective, ProjectProgressBarModule } from 'projects/ngx-xchange-ui/src/public-api';
+import {
+  ProfileTagsDirective,
+  ProjectProgressBarModule,
+} from 'projects/ngx-xchange-ui/src/public-api';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [Headline300Directive,XcIconArrowRightComponent,ProjectProgressBarModule,ProfileTagsDirective,Headline500Directive],
+  imports: [
+    Headline300Directive,
+    XcIconArrowRightComponent,
+    ProjectProgressBarModule,
+    ProfileTagsDirective,
+    Headline500Directive,
+    NgIf,
+    XcIconCheckComponent,
+    XcIconCloseSComponent
+  ],
   templateUrl: './project-modal.component.html',
-  styleUrl: './project-modal.component.scss'
+  styleUrl: './project-modal.component.scss',
 })
 export class ProjectModalComponent {
   @Input() project: any;
+  @Input() invitation: boolean = true;
   @Output() projectClick = new EventEmitter<any>();
 
   onProjectClick(project: any): void {
