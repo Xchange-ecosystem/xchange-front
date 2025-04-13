8c75f647e697d18669cca0124412096b2d1a2b2c
Damian Tapia <damian.tapia45@gmail.com>
2025-04-13 13:27:06 -0600

    style:feed page completed

--- projects/ngx-xchange-ui/src/components/header-title/header-title.component.ts
+++ projects/ngx-xchange-ui/src/components/header-title/header-title.component.ts
@@ -1,15 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { XcIconEditComponent } from '@indziaki/ngx-xchange-icons';
 
 @Component({
   selector: 'xc-header-title',
   standalone: true,
-  imports: [XcIconEditComponent],
+  imports: [XcIconEditComponent,NgIf],
   templateUrl: './header-title.component.html',
   styleUrl: './header-title.component.css'
 })
 export class HeaderTitleComponent {
   @Input() title: string = '';
+  @Input() isEditable: boolean = false;
   @Output() editClicked: EventEmitter<void> = new EventEmitter<void>();
 
   onEditClick(): void {
--- src/app/pages/feed/feed-header/feed-header.component.ts
+++ src/app/pages/feed/feed-header/feed-header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderBackgroundComponent } from 'projects/ngx-xchange-ui/src/components/header-background/header-background.component';
+import { HeaderCardsComponent } from '../header-cards/header-cards.component';
+import { RoundedImageModule } from '@indziaki/ngx-xchange-ui';
 
 @Component({
   selector: 'app-feed-header',
   standalone: true,
-  imports: [HeaderBackgroundComponent],
+  imports: [HeaderBackgroundComponent,HeaderCardsComponent,RoundedImageModule],
   templateUrl: './feed-header.component.html',
   styleUrl: './feed-header.component.scss'
 })
