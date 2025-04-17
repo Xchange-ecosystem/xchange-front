93e90a1c77958467fad7b0eef34b0b94975033dc
Damian Tapia <damian.tapia45@gmail.com>
2025-04-16 23:59:46 -0600

    style: edit title working

--- projects/ngx-xchange-ui/src/components/header-title/header-title.component.ts
+++ projects/ngx-xchange-ui/src/components/header-title/header-title.component.ts
@@ -1,20 +1,44 @@
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { XcIconEditComponent } from '@indziaki/ngx-xchange-icons';
+import { FormsModule } from '@angular/forms';
+import { XcIconCheckComponent, XcIconEditComponent, XcIconTimesComponent } from '@indziaki/ngx-xchange-icons';
+import { HeadlineStylesDirective } from '../../directives/headline-styles/headline-styles.directive';
 
 @Component({
   selector: 'xc-header-title',
   standalone: true,
-  imports: [XcIconEditComponent,NgIf],
+  imports: [
+    XcIconEditComponent,
+    NgIf,
+    CommonModule,
+    FormsModule,
+    XcIconTimesComponent,
+    XcIconCheckComponent,
+    HeadlineStylesDirective
+  ],
   templateUrl: './header-title.component.html',
   styleUrl: './header-title.component.css'
 })
 export class HeaderTitleComponent {
   @Input() title: string = '';
   @Input() isEditable: boolean = false;
-  @Output() editClicked: EventEmitter<void> = new EventEmitter<void>();
+  @Output() titleChange = new EventEmitter<string>();
+
+  editing = false;
+  tempTitle: string = '';
 
   onEditClick(): void {
-    this.editClicked.emit();
+    this.tempTitle = this.title;
+    this.editing = true;
+  }
+
+  onCancelClick(): void {
+    this.editing = false;
+  }
+
+  onSaveClick(): void {
+    this.title = this.tempTitle.trim();
+    this.titleChange.emit(this.title);
+    this.editing = false;
   }
 }
