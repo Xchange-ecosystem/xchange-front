17ff32d862eacde42c0ee3a1ec605e6b139028ab
Damian Tapia <damian.tapia45@gmail.com>
2025-04-22 18:04:45 -0600

    style: review #2 on profile page

--- src/app/pages/projects/project-modal/project-modal.component.ts
+++ src/app/pages/projects/project-modal/project-modal.component.ts
@@ -2,7 +2,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { XcIconArrowRightComponent, XcIconCheckComponent, XcIconCloseSComponent } from '@indziaki/ngx-xchange-icons';
 import { HeadlineStylesDirective } from 'projects/ngx-xchange-ui/src/directives/headline-styles/headline-styles.directive';
-import {ProfileTagsDirective,ProjectProgressBarModule,} from 'projects/ngx-xchange-ui/src/public-api';
+import {ButtonsWrappersDirective, ProfileTagsDirective,ProjectProgressBarModule,} from 'projects/ngx-xchange-ui/src/public-api';
 import { ProjectHeaderComponent } from '../project-header/project-header.component';
 export interface ProjectInterface {
   image: string;
@@ -30,6 +30,7 @@ export interface ProjectInterface {
     XcIconCheckComponent,
     XcIconCloseSComponent,
     ProjectHeaderComponent,
+    ButtonsWrappersDirective
   ],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.scss',
