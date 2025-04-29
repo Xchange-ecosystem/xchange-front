fba528d0e5b51474110a8a4c81cfb6d6e7d365a5
Damian Tapia <damian.tapia45gmail.com>
2025-04-28 18:11:21 -0600

    style: filter component created

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
+import { XcIconAmbulanceComponent } from '@indziaki/ngx-xchange-icons';
 import { CopyStylesDirective, HeadlineStylesDirective } from '@indziaki/ngx-xchange-ui';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     CopyStylesDirective,
-    HeadlineStylesDirective
+    HeadlineStylesDirective,
+    XcIconAmbulanceComponent,
+    FilterComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  options1 = [
+    {label:"test"},
+    {label:"test"},
+    {label:"test"}
+  ]
+  options2 = [
+    {label:"test"},
+    {label:"test"},
+    {label:"test"}
+  ]
+  options3 = [
+    {label:"test"},
+    {label:"test"},
+    {label:"test"}
+  ]
 
+  optionSelected(value:any){
+    console.log("seleccion hecha",value)
+  }
 }
