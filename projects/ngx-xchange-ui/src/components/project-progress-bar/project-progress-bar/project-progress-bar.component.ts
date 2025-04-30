a152587581fa6262e22b9040b004a4b46efc615e
manuXchange <josemanuelcabrera2011@gmail.com>
2025-04-29 23:16:12 -0600

    fix behavior and minwith project progress bar

--- projects/ngx-xchange-ui/src/components/project-progress-bar/project-progress-bar/project-progress-bar.component.ts
+++ projects/ngx-xchange-ui/src/components/project-progress-bar/project-progress-bar/project-progress-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'xc-project-progress-bar',
   templateUrl: './project-progress-bar.component.html',
-  styleUrls: ['./project-progress-bar.component.css']
+  styleUrls: ['./project-progress-bar.component.css'],
 })
 export class ProjectProgressBarComponent {
   @Input() completed: number = 0;
@@ -17,44 +17,46 @@ export class ProjectProgressBarComponent {
   public mouseMarkersActionObjectives: boolean = false;
 
   getSize(amount: number, total: number): string {
-    return String((100 / total ) * amount ) + '%'
+    return String((100 / total) * amount) + '%';
   }
 
-      // --xc-categories-progress: #FAE651; 
-    // --xc-categories-suggestion: #AF71F1;
-    // --xc-categories-completed: #1ccd66;
-    // --xc-grey200: #DBE0DC;
-    // bg-[var(--xc-grey200)]
-  get getCompleteClass() {
-    
-    let  rigthSideColor = 'bg-[var(--xc-grey200)]'
-    if (this.open > 0) 
-      rigthSideColor = 'bg-[var(--xc-categories-progress)]'
-    else if (this.suggested > 0) 
-      rigthSideColor = 'bg-[var(--xc-categories-suggestion)]'
-    const isRound = (this.completed !== this.objectivesInProject) ? '': ''
-   return `${rigthSideColor} ${isRound} h-full rounded-l-full ` 
+  // --xc-categories-progress: #FAE651;
+  // --xc-categories-suggestion: #AF71F1;
+  // --xc-categories-completed: #1ccd66;
+  // --xc-grey200: #DBE0DC;
+  // bg-[var(--xc-grey200)]
+  getCompleteClass() {
+    return this.completed !== this.objectivesInProject
+      ? 'rounded-e-lg'
+      : 'rounded-lg';
   }
-  getOpenClass(objectives: number, completeObjectives: number){
-    const rigthSideColor = this.suggested > 0 ? 'bg-[var(--xc-categories-suggestion)]': 'bg-[var(--xc-grey200)]'
-    const isRound = ((this.completed + this.open + this.suggested) === this.objectivesInProject) ? 'rounded-lg':''
-    return completeObjectives === objectives ? 
-    `${rigthSideColor} ${isRound} h-full` :
-    `${rigthSideColor} rounded-l-lg h-full`
+  getOpenClass(objectives: number, completeObjectives: number) {
+    const rightSideColor = this.completed > 0 ? 'bg-Main_accent' : '';
+    const isRound =
+      this.completed + this.open + this.suggested == this.objectivesInProject
+        ? 'rounded-lg'
+        : '';
+    return completeObjectives === objectives
+      ? `${rightSideColor} ${isRound} h-full`
+      : `${rightSideColor} ${isRound}  h-full`;
   }
 
   get SuggestedClass() {
-    if(this.suggested > 0) 
-      return 'bg-[var(--xc-grey200)]'
-    else return ''
-    // return this.objectivesInProject === this.suggested ? 
-    // "bg-Second_accent rounded-lg h-full" :
-    // "bg-Suggestion  rounded-l-lg h-full"
+    return this.suggested && this.completed > 0 && this.open === 0 ?
+    "bg-Main_accent h-full" :
+    this.suggested && this.open > 0 ?
+    "bg-[var(--xc-categories-progress)]  h-full" :
+    ''
   }
   getTotalClass(objectivesOpen: number, objectivesComplete: number) {
-    return objectivesOpen === 0 && objectivesComplete && this.suggested === 0 ? 
-    "bg-[var(--xc-grey200)] rounded-lg h-full flex items-center justify-center":
-    "bg-[var(--xc-grey200)] rounded-e-lg h-full flex items-center justify-center"
+    return objectivesOpen === 0 && objectivesComplete === 0 && this.suggested === 0
+      ? 'bg-[var(--xc-grey200)] rounded-lg h-full flex items-center justify-center'
+      : this.suggested > 0
+      ? 'bg-[var(--xc-categories-suggestion)] rounded-s-lg h-full flex items-center justify-center'
+      : objectivesOpen === 0 && objectivesComplete > 0 && this.suggested === 0
+      ? 'bg-Main_accent rounded-s-lg h-full flex items-center justify-center'
+      : objectivesOpen > 0 && this.suggested === 0
+      ? 'bg-[var(--xc-categories-progress)] rounded-s-lg h-full flex items-center justify-center'
+      : '';
   }
-
 }
