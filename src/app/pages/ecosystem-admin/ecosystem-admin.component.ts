ba5871e0ca357ced28c0ce953baea05e5bbfbbe9
Damian Tapia <damian.tapia45gmail.com>
2025-04-30 17:43:30 -0600

    add: content to page, buttons working style pending

--- src/app/pages/ecosystem-admin/ecosystem-admin.component.ts
+++ src/app/pages/ecosystem-admin/ecosystem-admin.component.ts
@@ -1,12 +1,27 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProfileTagsDirective } from '@indziaki/ngx-xchange-ui';
 
 @Component({
   selector: 'app-ecosystem-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    ProfileTagsDirective,
+    NgIf,
+    NgClass,
+    NgFor
+  ],
   templateUrl: './ecosystem-admin.component.html',
   styleUrl: './ecosystem-admin.component.scss'
 })
 export class EcosystemAdminComponent {
+  isNotification = false;
 
+  sections = ['Apperance', 'Legal', 'Credits', 'Templates', 'Members'];
+
+  section: string = 'Apperance';
+
+  changeSection(section: string): void {
+    this.section = section;
+  }
 }
