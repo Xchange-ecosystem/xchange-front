aad63dc977e29d6a35fc7a91071d9d4c3247b6ef
Damian Tapia <damian.tapia45@gmail.com>
2025-05-05 18:46:27 -0600

    add: templates page validation pending

--- src/app/pages/ecosystem-admin/ecosystem-admin.component.ts
+++ src/app/pages/ecosystem-admin/ecosystem-admin.component.ts
@@ -36,7 +36,13 @@ export class EcosystemAdminComponent {
   isSuspendMember = false;
   tempImage = "https://lh3.googleusercontent.com/a-/ALV-UjWBPEgImeAd-djw96NL4WF8xLI_JH3DbdkDsm-mmc7JjuXTCTXOZA8RbLgqtKSBbj9wgNTzghEZGdrJd0qKXkl7Qk8eROaPBrzCa_-HevwHm24u8r0VbtRCI8U7Mitcn404HcQ3Q_Z5OOanSyxk5RVJmvBkOrBASppT7mfsyfaLpa__0REuARYQW_kuqBQ2XusZcvaAEzvZ9hZ76ISVWZpubpwahABqOKV7tOX0iWGklH6hErJU--8aZyTSxm5AAiJed2gonzdz4t515lrNWGVpdGCScjvAzUXZ3U6Wh3boiyICBGxfMT-gtTBxWlfWNRJV3dsc5kYiIOilnU61ub10pZKqfO4x5GFIsxlQqz9EAZg6buZtgYtKgGqO_mK59cgogeDJb_njHxVpMoL3Zp5u1tG6qSWPNv539xY88ffW3J34jV6BXkMEO3MFKD3UiRV6AAbWe58gbAjjoK87nhus59oiDdDA2pKxwRN5OKN78ecdK4kepBJuSuNoLwgpPn624UeTei3v8b03Be9FNO6BaFkhB5hguYQDkMAN5mGwLTXbcZzrIb9EM0ImpluHV-_1cNvZ2w2KYR6y7GT-zuqa9fUiww3DO4jVyIH0GT0IB8V8X-Iog968OImfHSlrQSu0Co6j3XTGs0_nH9vTP62xPM3ZDtp6LdQf3ZacBjPI07bCDGGiznVQ52KPhHfX5uefcvdSy8hZKLSiOi22r5HrtrzAVr6ads4jLahOyxt8FlS8S2mxdughUqhBom4tP-WSP7E4GE9IZai3tTHrI8zXApsunxtLlqSgs5twmWJPxia2OcMSHbNVtFo7cCdnCXZ4eaSU9iRyfs4IQLuZks1soVhXJa3ZxkebOxjqnLeOqmBNbSH-BGFVSBNdVXzVdLOrrywyaLfMENyRRCY7XJAxefi3f9K-bOmwBP3llMgCX7QkjRH_BfEzaFUCNG8khfusV9ZP8qWqLRXbhNroGeAfVoYAE4_B=s64-w64-h64-c-k-no";
 
-  sections = ['Apperance', 'Legal', 'Credits', 'Templates', 'Members'];
+  sections = [
+    'Apperance', 
+    'Legal', 
+    'Credits', 
+    'Templates', 
+    'Members'
+  ];
   tags = [
     "Technology",
     "Health",
@@ -67,7 +73,7 @@ export class EcosystemAdminComponent {
     "MongoDB",
     "Node.js"
   ];
-  Transactions = [
+  transactions = [
     {
       name: "Manuel Cabrera",
       date: "14 Aug.24",
@@ -153,7 +159,100 @@ export class EcosystemAdminComponent {
       destination: "Investor Rewards"
     }
   ];
-  
+  templateCategories = [
+    {
+      title: 'Innovation Project',
+      selectAll: false,
+      options: [
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Open Innovation', selected: false }
+      ]
+    },
+    {
+      title: 'Change and Development',
+      selectAll: false,
+      options: [
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Open Innovation', selected: false }
+      ]
+    },
+    {
+      title: 'Assessments/ Audits',
+      selectAll: false,
+      options: [
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Open Innovation', selected: false },
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+      ]
+    },
+    {
+      title: 'Learning',
+      selectAll: false,
+      options: [
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Open Innovation', selected: false },
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+        { label: 'Open Innovation', selected: false },
+        { label: 'Startups', selected: true },
+        { label: 'Innovation Projects', selected: false },
+        { label: 'Corporate Innovation', selected: false },
+        { label: 'Research', selected: false },
+        { label: 'Academy & Curriculum', selected: false },
+        { label: 'Social Innovation', selected: false },
+        { label: 'Environmental Innovation', selected: false },
+      ]
+    },
+  ];
 
   section: string = 'Templates';
 
