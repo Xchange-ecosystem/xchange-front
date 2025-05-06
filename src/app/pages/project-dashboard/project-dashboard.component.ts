c027874108975d3d0773077d64b85f3cede4cce7
Damian Tapia <damian.tapia45@gmail.com>
2025-05-06 10:57:19 -0600

    feature: project dashboard page

---
+++ src/app/pages/project-dashboard/project-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-dashboard.component.html',
+  styleUrl: './project-dashboard.component.scss'
+})
+export class ProjectDashboardComponent {
+
+}
