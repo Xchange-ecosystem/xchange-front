c15084cb180414da1d681d16ab5ad7cbe9637771
Damian Tapia <damian.tapia45gmail.com>
2025-05-12 12:07:04 -0600

    add: modal components created

---
+++ src/app/pages/project-goals/new-goal-modal/new-goal-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-goal-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-goal-modal.component.html',
+  styleUrl: './new-goal-modal.component.scss'
+})
+export class NewGoalModalComponent {
+
+}
