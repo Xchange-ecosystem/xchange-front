3b14bf1025f6f8c8bcebec10242759ea44f5aba4
Damian Tapia <damian.tapia45gmail.com>
2025-05-12 13:20:19 -0600

    add: description modal

--- src/app/pages/project-goals/project-goals.component.ts
+++ src/app/pages/project-goals/project-goals.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { XcIconPlusComponent } from '@indziaki/ngx-xchange-icons';
 import { GoalsDropdownComponent } from './goals-dropdown/goals-dropdown.component';
+import { DescriptionModalComponent } from './description-modal/description-modal.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-project-goals',
   standalone: true,
   imports: [
+    NgIf,
     XcIconPlusComponent,
-    GoalsDropdownComponent
+    GoalsDropdownComponent,
   ],
   templateUrl: './project-goals.component.html',
   styleUrl: './project-goals.component.scss'
