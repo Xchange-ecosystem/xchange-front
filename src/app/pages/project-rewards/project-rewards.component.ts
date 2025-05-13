6cdb8e6a4c6cbbf865512870fdbb9520b72d409f
Damian Tapia <damian.tapia45gmail.com>
2025-05-13 10:58:30 -0600

    feature:  project rewards page added w/header

---
+++ src/app/pages/project-rewards/project-rewards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-rewards',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-rewards.component.html',
+  styleUrl: './project-rewards.component.scss'
+})
+export class ProjectRewardsComponent {
+
+}
--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -9,48 +9,52 @@ const routes: Routes = [
   {
     path: '',
     component: PublicComponent,
-    children: 
-    [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'login', component: LoginComponent },
-      {
-        path:'feed',
-        loadComponent: () => import('src/app/pages/feed/feed.component').then(m => m.FeedComponent),
-      },
-      {
-        path:'community',
-        loadComponent: () => import('src/app/pages/community/community.component').then(m => m.CommunityComponent),
-      },
-      {
-        path:'projects',
-        loadComponent: () => import('src/app/pages/projects/projects.component').then(m => m.ProjectsComponent),
-      },
-      {
-        path:'profile',
-        loadComponent: () => import('src/app/pages/profile/profile.component').then(m => m.ProfileComponent),
-      },
-      {
-        path:'dashboard',
-        loadComponent: () => import('src/app/pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
-      },
-      {
-        path:'ecosystem-admin',
-        loadComponent: () => import('src/app/pages/ecosystem-admin/ecosystem-admin.component').then(m => m.EcosystemAdminComponent),
-      },
-      {
-        path:'project-dashboard',
-        loadComponent: () => import('src/app/pages/project-dashboard/project-dashboard.component').then(m => m.ProjectDashboardComponent),
-      },
-      {
-        path:'project-settings',
-        loadComponent: () => import('src/app/pages/project-settings/project-settings.component').then(m => m.ProjectSettingsComponent),
-      },
-      {
-        path:'project-goals',
-        loadComponent: () => import('src/app/pages/project-goals/project-goals.component').then(m => m.ProjectGoalsComponent),
-      },
-    ],
+    children:
+      [
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', component: HomeComponent },
+        { path: 'login', component: LoginComponent },
+        {
+          path: 'feed',
+          loadComponent: () => import('src/app/pages/feed/feed.component').then(m => m.FeedComponent),
+        },
+        {
+          path: 'community',
+          loadComponent: () => import('src/app/pages/community/community.component').then(m => m.CommunityComponent),
+        },
+        {
+          path: 'projects',
+          loadComponent: () => import('src/app/pages/projects/projects.component').then(m => m.ProjectsComponent),
+        },
+        {
+          path: 'profile',
+          loadComponent: () => import('src/app/pages/profile/profile.component').then(m => m.ProfileComponent),
+        },
+        {
+          path: 'dashboard',
+          loadComponent: () => import('src/app/pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        },
+        {
+          path: 'ecosystem-admin',
+          loadComponent: () => import('src/app/pages/ecosystem-admin/ecosystem-admin.component').then(m => m.EcosystemAdminComponent),
+        },
+        {
+          path: 'project-dashboard',
+          loadComponent: () => import('src/app/pages/project-dashboard/project-dashboard.component').then(m => m.ProjectDashboardComponent),
+        },
+        {
+          path: 'project-settings',
+          loadComponent: () => import('src/app/pages/project-settings/project-settings.component').then(m => m.ProjectSettingsComponent),
+        },
+        {
+          path: 'project-goals',
+          loadComponent: () => import('src/app/pages/project-goals/project-goals.component').then(m => m.ProjectGoalsComponent),
+        },
+        {
+          path: 'project-rewards',
+          loadComponent: () => import('src/app/pages/project-rewards/project-rewards.component').then(m => m.ProjectRewardsComponent),
+        },
+      ],
   },
 ];
 
@@ -58,4 +62,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PublicRoutingModule {}
+export class PublicRoutingModule { }
