d2b3b6d3b6d0c9c9fc3051c55ef3950a60a6ef10
Damian Tapia <damian.tapia45@gmail.com>
2025-05-15 12:49:29 -0600

    add: employee fund page

--- src/app/pages/project-rewards/employee-fund/employee-fund.component.ts
+++ src/app/pages/project-rewards/employee-fund/employee-fund.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { XcIconArrowUpRightComponent, XcIconSaveComponent, XcIconTimesComponent, XcIconUserPlusComponent, XcIconUserXComponent } from '@indziaki/ngx-xchange-icons';
+import { RoundedImageModule } from '@indziaki/ngx-xchange-ui';
 
 @Component({
   selector: 'app-employee-fund',
   standalone: true,
-  imports: [],
+  imports: [
+    RoundedImageModule,
+        XcIconTimesComponent,
+        XcIconSaveComponent,
+        XcIconArrowUpRightComponent,
+        XcIconUserPlusComponent,
+        XcIconUserXComponent
+  ],
   templateUrl: './employee-fund.component.html',
   styleUrl: './employee-fund.component.scss'
 })
