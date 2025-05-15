1368fc61f4e87fe3da2268751e1fdafa4e2bd043
Damian Tapia <damian.tapia45@gmail.com>
2025-05-15 15:25:13 -0600

    style: Owners fund layout fixed

--- src/app/pages/project-rewards/owners-fund/owners-fund.component.ts
+++ src/app/pages/project-rewards/owners-fund/owners-fund.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RoundedImageModule } from "@indziaki/ngx-xchange-ui";
+import { XcIconArrowUpRightComponent } from "@indziaki/ngx-xchange-icons";
+import { XcIconSaveComponent } from "@indziaki/ngx-xchange-icons";
 
 @Component({
   selector: 'app-owners-fund',
   standalone: true,
-  imports: [],
+  imports: [RoundedImageModule, XcIconArrowUpRightComponent],
   templateUrl: './owners-fund.component.html',
   styleUrl: './owners-fund.component.scss'
 })
