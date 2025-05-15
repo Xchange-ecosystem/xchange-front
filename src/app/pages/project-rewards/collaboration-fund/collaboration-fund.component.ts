a0dd952b34d1af465c6cb8334270644d7059b549
Damian Tapia <damian.tapia45@gmail.com>
2025-05-14 21:19:01 -0600

    style: set fund & collaboration page styled

--- src/app/pages/project-rewards/collaboration-fund/collaboration-fund.component.ts
+++ src/app/pages/project-rewards/collaboration-fund/collaboration-fund.component.ts
@@ -1,12 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { XcIconArrowToRightComponent, XcIconArrowUpRightComponent, XcIconFileUploadComponent, XcIconPaperclipComponent, XcIconSaveComponent, XcIconTimesComponent } from '@indziaki/ngx-xchange-icons';
+import { ProfileTagsDirective, RoundedImageModule } from '@indziaki/ngx-xchange-ui';
 
 @Component({
   selector: 'app-collaboration-fund',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    ProfileTagsDirective,
+    XcIconFileUploadComponent,
+    XcIconPaperclipComponent,
+    XcIconTimesComponent,
+    XcIconSaveComponent,
+    XcIconArrowUpRightComponent,
+    RoundedImageModule
+  ],
   templateUrl: './collaboration-fund.component.html',
   styleUrl: './collaboration-fund.component.scss'
 })
 export class CollaborationFundComponent {
-
+  isFundSet = true;
 }
