0f59765a41ed34f29e674b964886d4115a2799c6
Damian Tapia <damian.tapia45@gmail.com>
2025-05-19 16:14:38 -0600

    delete: comments removed

--- src/app/pages/onboarding/investor-splash/investor-splash.component.ts
+++ src/app/pages/onboarding/investor-splash/investor-splash.component.ts
@@ -10,21 +10,16 @@ import { StepperService } from '../services/stepper-service/stepper.service';
   styleUrl: './investor-splash.component.scss'
 })
 export class InvestorSplashComponent {
-  /** Número de pasos que llega desde StepperService */
   steps = 0;
-
-  /** Suscripción para limpiar en ngOnDestroy */
   private sub!: Subscription;
 
   constructor(private stepperService: StepperService) {}
 
   ngOnInit(): void {
-    // Nos suscribimos al observable reactivo de steps
     this.sub = this.stepperService.getStepCount$().subscribe(count => {
       this.steps = count;
     });
 
-    // Por si el valor ya existe antes de la primera emisión
     this.steps = this.stepperService.getCurrentStepCount();
   }
 
