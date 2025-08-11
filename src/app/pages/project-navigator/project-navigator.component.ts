a0233a41281f3deb4f4ef453b0acd64df4494f1f
Damian Tapia <damian.tapia45@gmail.com>
2025-08-11 13:58:57 -0600

    add: filters responsiveness according to figma on navigator

--- src/app/pages/project-navigator/project-navigator.component.ts
+++ src/app/pages/project-navigator/project-navigator.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NavigatorFilterSelectorModule,NavigatorObjectiveStatusFilterModule,NavigatorRangeFilterModule,NavigatorUsersFilterModule } from '@indziaki/ngx-xchange-ui';
 
 @Component({
   selector: 'app-project-navigator',
   standalone: true,
-  imports: [],
+  imports: [
+    NavigatorFilterSelectorModule,
+    NavigatorUsersFilterModule,
+    NavigatorObjectiveStatusFilterModule,
+    NavigatorRangeFilterModule
+  ],
   templateUrl: './project-navigator.component.html',
   styleUrl: './project-navigator.component.scss'
 })
 export class ProjectNavigatorComponent {
-
+  collaborator: any[] = [
+    { id: 1, name: 'User 1' },
+    { id: 2, name: 'User 2' },
+    { id: 3, name: 'User 3' }
+  ];
 }
